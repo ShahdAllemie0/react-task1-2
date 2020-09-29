79fe5b99804a8c3d6d8f8a34ecfe2d49529ab4ab
Shahd Allemie <shahdallemie@Shahds-MacBook-Pro.local>
2020-09-29 14:41:02 +0300

    completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import items from './items';
+import styles from './styles';
 
 function App() {
+  const itemList=items.map((item) =>(
+      <div style={styles.item}key={item.id}>
+        <img style={styles.birdImage} src={item.image} alt={item.name}/>
+  <p style={styles.text}>{item.name}</p>
+  <p style={styles.text}>{item.price} SAR</p>
+
+      </div>
+    ));
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={styles.body}>
+      <h1 style={styles.text}>If and only if you are intersting to birds stuff</h1>
+      <p style={styles.text}>then</p>
+      <h3 style={styles.text}>Welcome to <b>SHAHD</b> shop ..</h3>
+      <div style={styles.list}>{itemList}</div>
+    
     </div>
   );
 }
